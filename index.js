f109723df3782c50df1090c5e8fc25b154773fc9
unknown <frist410@gmail.com>
2022-07-04 02:47:07 +0600

    add pg and db.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require('express')
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send('hello')
+})
+
+app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`))
